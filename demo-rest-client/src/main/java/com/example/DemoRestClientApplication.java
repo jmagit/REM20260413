dbe8acee50b00a53fc4de36cb17710aaf9f9f3c9
Javier <35864946+jmagit@users.noreply.github.com>
2026-04-22 23:33:12 +0200

    cliente sincrono flux

--- demo-rest-client/src/main/java/com/example/DemoRestClientApplication.java
+++ demo-rest-client/src/main/java/com/example/DemoRestClientApplication.java
@@ -1,10 +1,12 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -12,10 +14,23 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import com.example.models.Actor;
 
 @SpringBootApplication
-public class DemoClientRestApplication implements CommandLineRunner {
+public class DemoRestClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoClientRestApplication.class, args);
+		SpringApplication.run(DemoRestClientApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner cliente() {
+		return _ -> {
+		var jsonClient = RestClient.builder().defaultHeader("accept", "application/json").build();
+		System.out.println(
+				jsonClient.get()
+					.uri("http://localhost:8010/actores/v1/1")
+					.retrieve()
+					.body(Actor.class)
+			);
+		};
 	}
 	
 	@Bean
@@ -33,23 +48,33 @@ public class DemoClientRestApplication implements CommandLineRunner {
         return factory.createClient(ActoresProxy.class);
     }
 	
-	@Override
-	public void run(String... args) throws Exception {
-		var jsonClient = RestClient.builder().defaultHeader("accept", "application/json").build();
-		System.out.println(
-				jsonClient.get()
-					.uri("http://localhost:8010/actores/v1/1")
-					.retrieve()
-					.body(Actor.class)
-			);
-	}
-
 	@Bean
 	CommandLineRunner remoto(ActoresProxy proxy) {
 		return _ -> {
 			proxy.getAll("largo").forEach(IO::println);
 			IO.println(proxy.getOne(1));
 //			proxy.getAllShort().forEach(IO::println);
+		};
+	}
+
+	static record ActorReactive(int actorId, String firstName, String lastName) {
+	}
+
+	@Bean
+	CommandLineRunner reactivo() {
+		return _ -> {
+			var jsonClient = RestClient.builder().defaultHeader("accept", "application/json").build();
+			System.out.println(
+					jsonClient.get()
+						.uri("http://localhost:8085/demos/datos/cabeceras")
+						.retrieve()
+						.body(String.class)
+				);
+			jsonClient.get()
+				.uri("http://localhost:8085/actores/v1")
+				.retrieve()
+				.body(new ParameterizedTypeReference<List<ActorReactive>>() {})
+				.forEach(IO::println);
 		};
 	}
 }
